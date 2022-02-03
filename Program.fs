29743ffcb34f53083603372e7aeafbb0ff5e160f
Georg Haaser <krauthaufen@awx.at>
2022-02-03 01:39:04 +0100

    calculating PI

--- Program.fs
+++ Program.fs
@@ -12,7 +12,7 @@ let main _ =
     
     use p = new Problem()
 
-    let b = p.AddParameterBlock [|1.0|]
+    use b = p.AddParameterBlock [|3.0|]
 
     p.AddCostFunction(1, b, fun b ->
         [|
@@ -22,12 +22,16 @@ let main _ =
 
     let res = 
         p.Solve {
-            print = true
+            print = false
             solverType = SolverType.DenseQr
             functionTolerance = 1E-20
             gradientTolerance = 1E-20
             parameterTolerance = 1E-20
             maxIterations = 100
         }
+
+    printfn "RESIDUAL: %A" res
+    printfn "ERROR: %A" (sin b.Result.[0])
+    printfn "RESULT: %A" b.Result.[0]
 
     0
