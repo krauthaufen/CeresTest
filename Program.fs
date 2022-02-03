0f072b50e450020ea1e7e54f7c1cb853f3ccacaf
Georg Haaser <krauthaufen@awx.at>
2022-02-03 01:28:04 +0100

    import

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿open Aardvark.Base
+open CeresSharp
+
+[<EntryPoint>]
+let main _ =
+    let oldFilter = IntrospectionProperties.UnpackNativeLibrariesFilter
+    IntrospectionProperties.UnpackNativeLibrariesFilter <- System.Func<_,_>(fun a ->
+        a.GetName().Name = "Ceres" || oldFilter.Invoke(a)
+    )
+
+    Aardvark.Init()
+    
+    use p = new Problem()
+
+    let b = p.AddParameterBlock [|1.0|]
+
+    p.AddCostFunction(1, b, fun b ->
+        [|
+            sin b.[0]
+        |]
+    )
+
+    let res = 
+        p.Solve {
+            print = true
+            solverType = SolverType.DenseQr
+            functionTolerance = 1E-20
+            gradientTolerance = 1E-20
+            parameterTolerance = 1E-20
+            maxIterations = 100
+        }
+
+    0
